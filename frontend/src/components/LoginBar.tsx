97436819e7335dfdc306c112b8b1cf66f4416cae
Hiroji <joegilbertdev@outlook.com>
2022-09-23 13:25:01 +0100

    1-user login frontend

---
+++ frontend/src/components/LoginBar.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "../App.css";
+import { useState } from "react";
+
+const LoginBar = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const changeUsername = (event: React.ChangeEvent) => {
+    const target = event.target as HTMLTextAreaElement;
+    setUsername(target.value);
+  };
+  const changePassword = (event: React.ChangeEvent) => {
+    const target = event.target as HTMLTextAreaElement;
+    setPassword(target.value);
+  };
+
+  const handleLogin = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    console.log("clicked");
+  };
+
+  return (
+    <header className="header_auth">
+      <form className="header_form">
+        <label className="login_item" htmlFor="username">
+          Username:{" "}
+        </label>
+        <input
+          className="login_item"
+          id="usernameInput"
+          type="text"
+          onChange={changeUsername}
+        ></input>
+        <label className="login_item" htmlFor="password">
+          Password:{" "}
+        </label>
+        <input
+          className="login_item"
+          id="passwordInput"
+          type="text"
+          onChange={changePassword}
+        ></input>
+        <button className="login_item login_button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </header>
+  );
+};
+
+export default LoginBar;
