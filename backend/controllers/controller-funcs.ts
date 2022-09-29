d4724e0666066314bda852646a212765c0ca80ca
Hiroji <joegilbertdev@outlook.com>
2022-09-29 13:25:37 +0100

    adding typescript features to existing code

--- backend/controllers/controller-funcs.ts
+++ backend/controllers/controller-funcs.ts
@@ -1,12 +1,18 @@
 import selectUser from "../models/models-funcs";
+import {Request, Response} from 'express';
 
-const postLogin = async (req: any, res: any) => {
+//objects
+interface loginResponseObject {
+  login_response : {username: string, outcome: string}
+};
+
+const postLogin = async (req: Request, res: Response) => {
   try {
     const username = req.body.username;
     const password = req.body.password;
 
     const data = await selectUser(username, password);
-    const responseObject = { login_response : {username: data.username, outcome: data.outcome} };
+    const responseObject: loginResponseObject = { login_response : {username: data.username, outcome: data.outcome} };
     console.log(responseObject)
     if (responseObject.login_response.outcome === "valid") {
     res.status(200).send(responseObject);
