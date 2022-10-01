5f8f050069d6cf4fcbf71515a79492e3a8532792
Hiroji <joegilbertdev@outlook.com>
2022-10-01 17:41:37 +0100

    user data hashed in seed test

--- backend/controllers/controller-funcs.ts
+++ backend/controllers/controller-funcs.ts
@@ -1,10 +1,10 @@
 import selectUser from "../models/models-funcs";
-import {Request, Response} from 'express';
+import { Request, Response } from "express";
 
 //objects
 interface loginResponseObject {
-  login_response : {username: string, outcome: string}
-};
+  login_response: { username: string; outcome: string };
+}
 
 const postLogin = async (req: Request, res: Response) => {
   try {
@@ -12,15 +12,17 @@ const postLogin = async (req: Request, res: Response) => {
     const password = req.body.password;
 
     const data = await selectUser(username, password);
-    const responseObject: loginResponseObject = { login_response : {username: data.username, outcome: data.outcome} };
-    console.log(responseObject)
+    const responseObject: loginResponseObject = {
+      login_response: { username: data.username, outcome: data.outcome },
+    };
+    console.log(responseObject);
     if (responseObject.login_response.outcome === "valid") {
-    res.status(200).send(responseObject);
-  } else if (responseObject.login_response.outcome === "invalid password") {
-    res.status(400).send(responseObject);
-  } else {
-    res.status(404).send(responseObject);
-  }
+      res.status(200).send(responseObject);
+    } else if (responseObject.login_response.outcome === "invalid password") {
+      res.status(400).send(responseObject);
+    } else {
+      res.status(404).send(responseObject);
+    }
   } catch (error) {
     throw error;
   }
--- backend/db/seeds/seed-test.ts
+++ backend/db/seeds/seed-test.ts
@@ -1,42 +1,18 @@
-import {db, client} from "../connection";
+import { db, client } from "../connection";
 import userData from "../data/test/users";
-
-/*
-//schemas
-//import mongoose from "mongoose";
-import { Schema } from "mongoose";
-import test from "node:test";
-var bcrypt = require("bcrypt-nodejs");
-
-
-var userSchema = new Schema({
-  username: String,
-  password: String,
-});
-
-// hash the password
-userSchema.methods.generateHash = function(password:string) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// checking if password is valid
-userSchema.methods.validPassword = function(password:string) {
-  return bcrypt.compareSync(password, this.password);
-};
-
-//test creating a user obj
-const HashedUser = mongoose.model('user', userSchema);
-const testUser:any = new HashedUser({username: "test", password: "test"});
-//test hashing password
-testUser.password = testUser.generateHash(testUser.password);
-
-console.log("test user", testUser)
-//test decrypt password
-const isValid = bcrypt.compareSync("password", testUser.password);
-console.log("check valid password", isValid)
-*/
+import bcrypt from "bcrypt";
+const saltRounds = 10; //move
 
 export async function seed() {
+
+  //hash password data for testing
+  const hashedCopyOfUserData = userData.map(async ({ ...user }) => {
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(user.password, salt);
+    return {username: user.username, password: hash}
+  });
+  const hashedUserData = await Promise.all(hashedCopyOfUserData);
+
   // Use connect method to connect to the server
   await client.connect();
   console.log("Connected successfully to server");
@@ -52,19 +28,24 @@ export async function seed() {
 
   //create collections
   const users = db.collection("users");
-  console.log("Created collection: users")
+  console.log("Created collection: users");
 
   //insert users data
-  const insertResult = await users.insertMany(userData);
-  //console.log('Inserted documents =>', insertResult);
+  const insertResult = await users.insertMany(hashedUserData);
+  console.log('Inserted documents =>', insertResult);
 
   const filteredDocs = await users.find({}).toArray();
-  //console.log('Found documents  =>', filteredDocs);
+  console.log('Found documents  =>', filteredDocs);
   //await client.close()
   //return "done.";
+
+  /*
+  const test = bcrypt.compareSync("passwor",filteredDocs[0].password);
+  console.log(test)
+  */
 };
 
-seed()
-  //.then(console.log)
-  //.catch(console.error)
-  //.finally(() => client.close());
+seed();
+//.then(console.log)
+//.catch(console.error)
+//.finally(() => client.close());
