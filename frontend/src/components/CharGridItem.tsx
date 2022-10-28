829c9f43914ca2e10cb534590b6576b70f4434ed
Hiroji <joegilbertdev@outlook.com>
2022-10-28 14:13:33 +0100

    each CharGridItem card has a useState toggle

--- frontend/src/components/CharGridItem.tsx
+++ frontend/src/components/CharGridItem.tsx
@@ -1,23 +1,34 @@
 import { useState } from "react";
 
-const CharGridItem = ({char}:any) => {
-    const {_id, name, ownerUsername, imgURL} =char;
+const CharGridItem = ({ char }: any) => {
+  const { _id, name, ownerUsername, imgURL } = char;
 
-    //states
-    const [isOpen, setIsOpen] = useState(false);
-    //implements component composition toggle here
+  //states
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <article className="charGrid__grid__item">
-                    <div>
-                  <img id="a"
-                    src={imgURL}
-                    alt={`Character name: ${name}, Owner name: ${ownerUsername}`}
-                  ></img>
-                  <p id="b">{char.name}</p>
-                  </div>
-                </article>
-    )
+  //functions
+  const toggleOpen = () => {
+    let inverseIsOpen = !isOpen;
+    setIsOpen(inverseIsOpen);
+  };
+
+  return (
+    <article
+      className="charGrid__grid__item"
+      onClick={() => {
+        toggleOpen();
+      }}
+    >
+      <div>
+        <img
+          id="a"
+          src={imgURL}
+          alt={`Character name: ${name}, Owner name: ${ownerUsername}`}
+        ></img>
+        <p id="b">{char.name}</p>
+      </div>
+    </article>
+  );
 };
 
 export default CharGridItem;
