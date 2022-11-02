bc444264e78fa58167bb9cd4799bc64ad444b1c8
Hiroji <joegilbertdev@outlook.com>
2022-11-02 12:58:46 +0000

    mobile optimisation of details grid

--- backend/db/data/test/chars.ts
+++ backend/db/data/test/chars.ts
@@ -39,7 +39,7 @@ const charsData: charData[] = [
     height: "140",
     weight: "70",
     imgURL: "https://risibank.fr/cache/medias/0/27/2795/279561/full.png",
-    bio: "",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     ownerUsername: "test2",
--- frontend/src/components/CharDetails.tsx
+++ frontend/src/components/CharDetails.tsx
@@ -9,18 +9,17 @@ const CharDetails = () => {
   if (!selectedCharacter) {
     return <section className="charGrid__details"></section>;
   } else {
-
     //destructure selectedCharacter
     const { name, ownerUsername, bio, _id, imgURL } = selectedCharacter;
     const keys = Object.keys(selectedCharacter);
     const uppercaseKeys = (key: string) => {
       const letters = key.split("");
       const capitalisedLetters = letters.map((letter, index) => {
-       return index === 0 ? letter.toUpperCase() : letter
+        return index === 0 ? letter.toUpperCase() : letter;
       });
       const newTitle = capitalisedLetters.join("");
-      return newTitle
-    }; 
+      return newTitle;
+    };
     keys.splice(keys.indexOf("_id"), 1);
     keys.splice(keys.indexOf("bio"), 1);
     keys.splice(keys.indexOf("imgURL"), 1);
@@ -28,31 +27,32 @@ const CharDetails = () => {
     return (
       <section className="charGrid__details">
         <div className="charGrid__details__item">
+          <img
+            id={_id}
+            src={imgURL}
+            alt={`Character name: ${name}, Owner name: ${ownerUsername}`}
+          ></img>
+        </div>
+        <div className="charGrid__details__item">
+          <h3>Biography</h3>
+          <p>{bio}</p>
+        </div>
+        <div className="charGrid__details__item">
           <h3>{name}</h3>
           <ul>
             {keys.map((key) => {
               return (
                 <li className="charGrid__details__catagory__li" key={key}>
-                  <p className="charGrid__details__title">{key === "ownerUsername" ? "Owner" : uppercaseKeys(key)}:</p>
                   <p className="charGrid__details__title">
+                    {key === "ownerUsername" ? "Owner" : uppercaseKeys(key)}:
+                  </p>
+                  <p className="charGrid__details__info">
                     {selectedCharacter[key]}
                   </p>
                 </li>
               );
             })}
           </ul>
-        </div>
-        <div className="charGrid__details__item">
-          <h3>Biography</h3>
-          <p>{bio}</p>
-        </div>
-
-        <div className="charGrid__details__item">
-          <img
-            id={_id}
-            src={imgURL}
-            alt={`Character name: ${name}, Owner name: ${ownerUsername}`}
-          ></img>
         </div>
       </section>
     );
