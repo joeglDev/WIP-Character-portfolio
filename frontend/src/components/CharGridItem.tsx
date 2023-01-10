01350531475754de663d499ca36939ffae0dd661
Hiroji <joegilbertdev@outlook.com>
2023-01-10 15:00:22 +0000

    CharGrisItem.tsx: added indexTab and enter key down events to improve keyboard navigation

--- frontend/src/components/CharGridItem.tsx
+++ frontend/src/components/CharGridItem.tsx
@@ -1,4 +1,4 @@
-import {  useContext } from "react";
+import { useContext } from "react";
 import { SelectedCharacterContext } from "../App";
 
 const CharGridItem = ({ char }: any) => {
@@ -8,21 +8,35 @@ const CharGridItem = ({ char }: any) => {
   const context = useContext(SelectedCharacterContext);
   const setSelectedCharacter = context.setSelectedCharacter;
 
+  //methods
+   /**
+    * Listens for a enter keydown event and passes image of event's char data to setSelectedCharacter context.
+    * 
+    * @param {React.KeyboardEvent<HTMLImageElement>} event - Keyboard event which is passed to function on keydown of img.
+    */
+   const handleEnterKeyDown = (event: React.KeyboardEvent<HTMLImageElement>) => {
+    if (event.key === 'Enter') {
+      setSelectedCharacter(char);
+    }
+  }
+
   return (
     <article
       className="charGrid__grid__item"
-      onClick={(() => {setSelectedCharacter(char)})}
+      onClick={() => {
+        setSelectedCharacter(char);
+      }}
     >
       <div>
         <img
+          tabIndex={0}
+          onKeyDown={handleEnterKeyDown}
           id="a"
           src={imgURL}
           alt={`Character name: ${name}, Owner name: ${ownerUsername}`}
         ></img>
         <p id="b">{char.name}</p>
       </div>
-
-     
     </article>
   );
 };
